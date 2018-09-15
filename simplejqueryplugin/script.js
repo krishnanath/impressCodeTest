60b78131d3384d1dfed9768fc3992c0a0dbd1eb3
krishnanath <isabella0484@gmail.com>
2018-09-15 11:12:41 +0530

    coding test

---
+++ simplejqueryplugin/script.js
@@ -0,0 +1,14 @@
+
+ $(document).ready(function(){
+     $("span").css("color","red");
+    
+    
+ });
+
+ $(document).ready(function(){
+    $("button").on({
+        mouseenter: function(){
+            $(this).css("color", "red");
+        }
+    });    
+});
